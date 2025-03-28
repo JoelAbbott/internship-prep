21b46b70ec03c27ef70b618aac37a91d2e343087
JoelAbbott <joelmichaelabbott@gmail.com>
2025-03-27 20:23:09 -0500

    Added pandas_sales_cleaner.py and sales.csv file

---
+++ week_01_python_basics/pandas_sales_cleaner.py
@@ -0,0 +1,16 @@
+#  🧠 Pseudocode
+# import pandas, logging, os
+# check for our .log folder
+# Configure our logging
+
+# Create a Function called clean_data(messy_data.csv, greater_less_dict)
+# Clean all empty rows
+# log cleaning success, error handling
+# loop through and print the rows where the Amount is >1000
+# keep a counter for rows with "Amount" < 1000
+# Keep a counter for rows with "Amount" > 1000
+# log >, < 1000 "Amounts"
+# print the count for > & < 1000
+# log summary results
+
+# Use main() Function to only call functions
